33f6d7b4978caea7c15056e0ae76d1af5732ff29
Shweta <shwetachalke731@>
2021-10-11 22:42:12 +0530

    first commit

---
+++ src/main/java/com/shwetsDemo/App.java
@@ -0,0 +1,13 @@
+package com.shwetsDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
